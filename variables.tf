b19b5e77f9da6dec1b1052b8c13aeac9ad30abb1
Mukesh Singh <102808932+mukesh9467@users.noreply.github.com>
2024-12-25 17:07:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "instance_count" {
-  type        = number
-  description = "Number of EC2 instances"
-}
-variable "subnet_ids" {
-  type        = list(string)
-  description = "List of Subnet IDs for EC2 instances"
-}
 variable "ami_id" {
   type        = string
   description = "AMI ID for EC2 instances"
@@ -17,8 +9,4 @@ variable "instance_type" {
 variable "ssh_key" {
   type = string
   description = "SSH key for login"
-}
-variable "sec_grp_id" {
-  type = string
-  description = "Security group to be used"
 }
