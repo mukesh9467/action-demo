c6adbcc6ef91a6997c360a75197e8b1d18fa99c7
Mukesh Singh <102808932+mukesh9467@users.noreply.github.com>
2024-12-25 17:27:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
   ami             = var.ami_id
   instance_type   = var.instance_type
-  subnet_id       = ["subnet-07644b9a6c62cadee"]
+  subnet_id       = "subnet-07644b9a6c62cadee"
   security_groups = ["sg-0826493edff1d99c8"]
   key_name        = var.ssh_key
  
